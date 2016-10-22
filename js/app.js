1a0f2d6ac8241e1d46cafe69ab518a72993986a4
Paulina Lei <paulina.y.lei@gmail.com>
2016-10-22 03:46:41 -0700

    added in more files and content

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
+// jQuery and magic.css
 $(document).ready(function() {
+  $(".headline").addClass('magictime puffIn');
+
   $("modal1").click(function() {
     $("#myModal").modal({
       backdrop: true
